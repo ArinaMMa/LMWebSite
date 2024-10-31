604b99abfe945049d88afeda00a7daa9d4668a74
ArinaMMa <arina.mancinimarlot@gmail.com>
2024-10-31 13:42:09 +0100

    add a scrolled title

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     const headerLogo = document.querySelector(".header-logo");
+    const headerTitle = document.querySelector(".header-title");
 
     if (headerLogo) {
         window.addEventListener("scroll", function () {
@@ -7,13 +8,29 @@ document.addEventListener("DOMContentLoaded", function () {
 
             if (window.scrollY > 50) {
                 headerLogo.classList.add("scrolled");
-                console.log("Class 'scrolled' ajoutée");
+                console.log("Class 'scrolled' ajoutée logo");
             } else {
                 headerLogo.classList.remove("scrolled");
-                console.log("Class 'scrolled' retirée");
+                console.log("Class 'scrolled' retirée logo");
             }
         });
     } else {
         console.error("L'élément .header-logo est introuvable");
+    }
+
+    if (headerTitle) {
+        window.addEventListener("scroll", function () {
+            console.log("Scrolling..."); // Vérification du défilement
+
+            if (window.scrollY > 50) {
+                headerTitle.classList.add("scrolled");
+                console.log("Class 'scrolled' ajoutée title");
+            } else {
+                headerTitle.classList.remove("scrolled");
+                console.log("Class 'scrolled' retirée title");
+            }
+        });
+    } else {
+        console.error("L'élément .header-title est introuvable");
     }
 });
